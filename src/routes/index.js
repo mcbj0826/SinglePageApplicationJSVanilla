8a774eef1f2e44292921a86a240a17c6704fb34a
John Fredy <john_fred_y26@hotmail.com>
2020-06-17 19:29:51 -0500

    Creando página 404 y haciendo render.

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+// Archivo que sirve para  importar los templates y páginas para luego hacer render
+
+import Header from '../templates/Header'
+import Home from '../pages/Home'
+import Character from '../pages/Character'
+import Error404 from '../pages/Error404'
+
+const routes = {
+    '/': Home,
+    '/:id': Character,
+    '/contact': 'Contact',
+}
+
+//Manejador que sirve para mostrar los elementos según la lógica que le demos
+
+
+const router = async () => {
+    const header = null || document.getElementById('header')
+    const content = null || document.getElementById('content')
+
+    header.innerHTML = await Header()
+
+}
+
+export default router
